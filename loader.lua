7d81284e18acbf6a06db171185ee0f726c205626
XVCHub <oanruc206@gmail.com>
2026-02-22 23:12:40 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,8 @@
+i = game.PlaceId
+if i == 15135285305 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XVCHub/Games/main/GEF"))()
+elseif i == 83907398368798 or i == 96168869671905 or i == 123535780262114 or i == 139520650328374 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XVCHub/Games/main/robit"))()
+else
+print'vro please join supported game'
+end
